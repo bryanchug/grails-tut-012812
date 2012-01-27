aab5d3588a979eee7231104da243b06f67d2f8ba
Bryan Chug <bryan.chug@gmail.com>
2012-01-27 21:21:47 +0800

    Connected DataSource to MySQL

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -13,8 +13,13 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE"
+		dbCreate = "update"
+		pooled = true
+		driverClassName = "com.mysql.jdbc.Driver"
+		username = "root"
+		password = "root"
+		url = "jdbc:mysql://localhost:3306/library"
+		dialect = 'org.hibernate.dialect.MySQL5InnoDBDialect'
         }
     }
     test {
